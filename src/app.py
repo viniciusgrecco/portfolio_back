80bc0fe1fdb6662a9fe316306d1c068eab8f32bb
viniciusgrecco <viniciusgfm@al.insper.edu.br>
2025-10-17 01:41:42 -0300

    fix: cors description

--- src/app.py
+++ src/app.py
@@ -49,6 +49,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://localhost:8080",
         "http://localhost:3000"
+        # Adicionar dominio apos deploy do front-end
     ],
     allow_credentials=True,
     allow_methods=["*"],
